a60cf8c027a2ec7a0adda5838bd300e05cbb2a52
rogeryeosgit <roger@geekshacking.com>
2018-02-12 16:21:02 +0800

    nb3

---
+++ src/fonts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class fontsImport extends React.Component {
+    async componentWillMount() {
+        await Expo.Font.loadAsync({
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+        });
+    }
+}
