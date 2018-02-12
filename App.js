89a7378bde01506c9271d3c56848ca94995c1924
rogeryeosgit <roger@geekshacking.com>
2018-02-12 16:37:27 +0800

    nb4

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 // import { StyleSheet, Text, View } from 'react-native';
-import fontImport from "./src/fonts.js";
+// import fontImport from "./src/fonts.js";
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 
 export default class App extends React.Component {
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+  }
+
   render() {
     return (
       <Container>
