fa97ac7840fac66a086291243f9315f96e1fac91
rogeryeosgit <roger@geekshacking.com>
2018-02-12 16:21:37 +0800

    nb3

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import { StyleSheet, Text, View } from 'react-native';
+import fontImport from "./src/fonts.js";
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 
 export default class App extends React.Component {
